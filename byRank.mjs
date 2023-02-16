26898203f178d29b3c027a4f2471fa536ec4336f
Ezell <lynellf@gmail.com>
2023-02-15 18:34:06 -0600

    account for character specific classes

--- byRank.mjs
+++ byRank.mjs
@@ -14,31 +14,50 @@ const withIdentifier = (keyname, arr) => (item, index) => ({
 
 const byCorrelationDesc = (a, z) => z.correlation - a.correlation;
 
-const withTopClasses = (keys, normalClasses, identifierKey) => (acc, item) => {
-  const name = item[identifierKey];
-  const charBaseStats = keys.map(asValue(item));
-  const classValues = normalClasses.map(asValues(keys));
+const withTopClasses =
+  (keys, normalClasses, identifierKey, exceptions) => (acc, item) => {
+    const name = item[identifierKey];
+    const charBaseStats = keys.map(asValue(item));
+    const characterSpecificClass = normalClasses.filter((c) =>
+      Boolean(
+        exceptions.find((e) => e.class === c.Name && e.character === name)
+      )
+    );
 
-  const topClasses = classValues
-    .map(asCorrelation(charBaseStats))
-    .map(withIdentifier(identifierKey, normalClasses))
-    .sort(byCorrelationDesc)
-    .slice(0, 5);
+    const withoutCharacterSpecificClasses = normalClasses.filter(
+      (c) => !Boolean(exceptions.find((e) => e.class === c.Name))
+    );
+    const mergedNormalClasses = withoutCharacterSpecificClasses.concat(
+      characterSpecificClass
+    );
+    const classValues = mergedNormalClasses.map(asValues(keys));
 
-  return {
-    ...acc,
-    [name]: topClasses
+    // console.log({
+    //   baseClasses: characterSpecificClass.map((c) => c.Name),
+    //   name
+    // });
+
+    const topClasses = classValues
+      .map(asCorrelation(charBaseStats))
+      .map(withIdentifier(identifierKey, mergedNormalClasses))
+      .sort(byCorrelationDesc)
+      .slice(0, 5);
+
+    return {
+      ...acc,
+      [name]: topClasses
+    };
   };
-};
 
 export default function byRank(
   keys = [],
   normalClasses = [],
   normalChars = [],
+  exceptions = [],
   identifierKey = "Name"
 ) {
   return normalChars.reduce(
-    withTopClasses(keys, normalClasses, identifierKey),
+    withTopClasses(keys, normalClasses, identifierKey, exceptions),
     {}
   );
 }
